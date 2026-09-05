a26e664b0cd2ebd350581330349518c074d2ec81
Muhammad Haseeb <mhaseeb04@users.noreply.github.com>
2026-09-05 15:00:46 +0000

    Refresh project identity and documentation

---
+++ README.md
@@ -0,0 +1,48 @@
+# 💼 Career Guidance Chatbot
+
+A chatbot that helps students to get career advice using Natural Language Processing (NLP) and Machine Learning. Built with Python, Streamlit, and scikit-learn.
+
+---
+
+## 🚀 Live Demo
+
+Explore the Career Guidance Chatbot in action!  
+Ask your career-related questions and receive smart, personalized advice powered by advanced NLP and machine learning techniques.
+
+[**Try the chatbot now →**](https://career-guidance-chatbot-tcsdhbh3njs3prbysrksya.streamlit.app/)
+
+---
+
+## 🧠 Features
+
+- 🔎 Understands user questions using vectorization and cosine similarity
+- 💬 Provides intelligent career advice from a custom dataset
+- 🧹 Cleans and preprocesses user input for better accuracy
+- 🖼 User-friendly interface built with Streamlit
+- ✅ Gives reliable answers or politely warns when unsure
+
+---
+
+## 🛠 Tech Stack
+
+- Python
+- Streamlit
+- Pandas
+- scikit-learn
+- NLTK
+- Joblib
+
+---
+
+## 🧾 Dataset
+
+A CSV file (CareerGuidanceDataset.csv) containing pairs of questions and answers.  
+Each question is preprocessed and vectorized to compare with user queries.
+
+---
+
+## 📬 Contact
+Developed by **Muhammad Haseeb**
+- 📧 Email: khizarishtiaq59@gmail.com
+- 🌐 GitHub: https://github.com/mhaseeb04
+
---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd 
+import joblib
+import string
+import nltk
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+english_stopwords = stopwords.words('english')
+
+# Load the CSV saved from Jupyter
+df = pd.read_csv('CareerGuidanceDataset.csv')
+
+# Setup
+st.set_page_config(page_title="Career Guidance Chatbot", layout="centered")
+st.title('🎓 Career Guidance Chatbot')
+
+# Career Categories 
+career_roles = sorted(df['role'].unique())
+selected_role = st.selectbox("🎯 Choose a Career Role", career_roles)
+
+# load model
+model = joblib.load('chatbot_model.pkl')
+
+# load vectorizer
+vectorizer = joblib.load('vectorizer.pkl')
+
+# Function for cleaning the text / Preprocessing
+def clean_text(text):
+    text = text.lower()
+    for p in string.punctuation:
+        text = text.replace(p, '')
+    return ' '.join([word for word in text.split() if word not in english_stopwords ])
+
+
+# Form to ask the  question from the user
+with st.form("chat_form"):
+    user_ques = st.text_input('Ask me a career-related question: ')
+    submitted = st.form_submit_button("🔍 Get Answer")
+
+
+# Filter dataset for selected role
+filtered_df = df[df['role'] == selected_role].copy()
+
+# Clean the filtered questions
+filtered_df['cleaned_question'] = filtered_df['question'].apply(clean_text)
+
+# Vectorize the cleaned questions
+filtered_dataset_ques = vectorizer.transform(filtered_df['cleaned_question'])
+
+
+# Apply logic and give answer to user
+if submitted and user_ques:
+    
+    # Clean the user question 
+    clean_que = clean_text(user_ques)
+    
+    # Convert the cleaned user question into vectors
+    user_question_vec = vectorizer.transform([clean_que])
+
+    # Finding the similarity between the user question and questions in dataset
+    similarity_scores = cosine_similarity(user_question_vec, filtered_dataset_ques)
+
+    best_index = similarity_scores.argmax()
+    best_score = similarity_scores[0, best_index]
+
+    if best_score > 0.3:
+        st.success("Chatbot Answer: " + filtered_df['answer'].iloc[best_index])
+    else:
+        st.error("\nChatbot: Sorry, I don't have enough information about that.")
+        
+        
+st.markdown(
+    """
+    <hr style="border: 1px solid #eaeaea; margin-top: 0px;">
+    <div style="text-align: center; font-size: 15px;">
+        👨‍💻 Developed by <b>Muhammad Haseeb</b><br>
+        🔗 <a href="https://linkedin.com/in/khizar-ishtiaq-716518315" target="_blank">Visit my LinkedIn Profile</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
